353acf07f2d3c75f9279450e520e40eca29fe17c
ppyom <ouob77@kakao.com>
2024-09-24 17:45:07 +0900

    design: Footer 스타일 추가

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,5 +1,21 @@
+import { FaGithub, FaRegEnvelope } from 'react-icons/fa6';
+import styles from './styles.module.css';
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.info}>
+        <a href="https://github.com/ppyom" target="_blank">
+          <FaGithub />
+        </a>
+        <button>
+          {/* TODO 메일주소 복사 기능 추가 필요 */}
+          <FaRegEnvelope />
+        </button>
+      </div>
+      <p className={styles.copyright}>ⓒ 2024. PPYOM. All rights reserved</p>
+    </footer>
+  );
 };
 
 export default Footer;
