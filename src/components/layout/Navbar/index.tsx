edde4dc84a56821cb4340e73bbcf1cce810b5c85
ppyom <ouob77@kakao.com>
2024-09-24 17:28:42 +0900

    design: Navbar 스타일 추가

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -1,5 +1,31 @@
+import { FiCalendar, FiCheckSquare } from 'react-icons/fi';
+import { IoTelescopeOutline } from 'react-icons/io5';
+import styles from './styles.module.css';
+
 const Navbar = () => {
-  return <nav>Nav</nav>;
+  return (
+    <nav className={styles.nav}>
+      {/* TODO router 추가한 다음 Link로 수정 필요 */}
+      <a href="#">
+        <div className={`${styles.item} ${true ? styles.active : ''}`}>
+          <FiCalendar />
+          <p>일정 관리</p>
+        </div>
+      </a>
+      <a href="#">
+        <div className={`${styles.item} ${false ? styles.active : ''}`}>
+          <FiCheckSquare />
+          <p>할 일</p>
+        </div>
+      </a>
+      <a href="#">
+        <div className={`${styles.item} ${false ? styles.active : ''}`}>
+          <IoTelescopeOutline />
+          <p>새로운 뉴스</p>
+        </div>
+      </a>
+    </nav>
+  );
 };
 
 export default Navbar;
