5ec088d81007a67a79eb4e2d2c62695eb322be84
ppyom <ouob77@kakao.com>
2024-09-24 16:19:58 +0900

    design: Sidebar 스타일 추가

--- src/components/layout/Sidebar/index.tsx
+++ src/components/layout/Sidebar/index.tsx
@@ -1,10 +1,11 @@
 import Header from '../Header';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
+import styles from './styles.module.css';
 
 const Index = () => {
   return (
-    <div>
+    <div className={styles.sidebar}>
       <Header />
       <Navbar />
       <Footer />
