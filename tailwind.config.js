363a5fac9f6376a67cbdcb50e865f40bb6796de8
ppyom <ouob77@kakao.com>
2024-09-24 12:12:58 +0900

    design: 기본 스타일 설정
    * TailwindCSS, PostCSS, Autoprefixer 설치

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          light: '#c5b4fb',
+          default: '#907ad6',
+          dark: '#4f518c',
+        },
+        base: {
+          white: '#ffffff',
+          black: '#000000',
+        },
+        service: {
+          success: '#31D555',
+          successLight: '#e6faea',
+          error: '#f52222',
+          errorLight: '#f9d2d2',
+        },
+        tags: {
+          javascript: '#ffe02f',
+          react: '#61dafb',
+          html: '#ff6827',
+          css: '#487fff',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
