f03cc806e57eebc849450e32dd9e59914cae0787
ppyom <ouob77@kakao.com>
2024-09-25 10:48:25 +0900

    fix: ComponentPropsWithoutRef 제대로 작동하도록 수정

--- src/components/common/MenuButton/index.tsx
+++ src/components/common/MenuButton/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { FiMenu } from 'react-icons/fi';
 import styles from './styles.module.css';
 
-interface Props extends React.ComponentPropsWithoutRef<HTMLButtonElement> {}
+interface Props extends React.ComponentPropsWithoutRef<'button'> {}
 
 const MenuButton = ({ className = '', ...props }: Props) => {
   return (
