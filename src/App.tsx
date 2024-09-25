22a0a931479537fd61853f0c54929402ea29b150
ppyom <ouob77@kakao.com>
2024-09-25 15:01:47 +0900

    feat: Toast 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { ToastProvider } from '@/context/ToastContext.tsx';
 import Sidebar from '@/components/layout/Sidebar';
 import MobileHeader from '@/components/layout/MobileHeader';
 import { routes } from '@/routes.ts';
@@ -7,7 +8,7 @@ import { routes } from '@/routes.ts';
 function App() {
   const [openSidebar, setOpenSidebar] = useState(false);
   return (
-    <>
+    <ToastProvider>
       <MobileHeader open={() => setOpenSidebar(true)} />
       <Sidebar isOpen={openSidebar} close={() => setOpenSidebar(false)} />
       <Routes>
@@ -15,7 +16,7 @@ function App() {
           <Route key={`route_${path}`} path={path} element={<PageElement />} />
         ))}
       </Routes>
-    </>
+    </ToastProvider>
   );
 }
 
