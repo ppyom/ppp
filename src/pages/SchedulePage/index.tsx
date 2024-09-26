945b8ff1dbd0988722da0dc368d7fc953390c6dd
ppyom <ouob77@kakao.com>
2024-09-26 09:34:26 +0900

    design: PageLayout 적용 #2

--- src/pages/SchedulePage/index.tsx
+++ src/pages/SchedulePage/index.tsx
@@ -1,5 +1,11 @@
+import PageLayout from '@/components/layout/PageLayout';
+
 const SchedulePage = () => {
-  return <>일정 관리</>;
+  return (
+    <PageLayout title="일정 관리" hideTitle>
+      일정 관리
+    </PageLayout>
+  );
 };
 
 export default SchedulePage;
