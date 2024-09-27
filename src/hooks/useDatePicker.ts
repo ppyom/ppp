97fbec58c135ecb757bec59c5575a56338e5e936
ppyom <ouob77@kakao.com>
2024-09-27 15:52:06 +0900

    style: 개발 끝난 로그 제거 #2

--- src/hooks/useDatePicker.ts
+++ src/hooks/useDatePicker.ts
@@ -3,7 +3,6 @@ import { useState } from 'react';
 const useDatePicker = (defaultDate: string) => {
   const [selected, setSelected] = useState<string>(defaultDate);
   const updateDate = (date: string) => {
-    console.log(date);
     setSelected(date);
   };
   return { selected, updateDate };
