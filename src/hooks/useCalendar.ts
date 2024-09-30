5f1ac7f1f04cacecb2d668b4eca362001801965f
ppyom <ouob77@kakao.com>
2024-09-30 11:03:52 +0900

    refactor: Date 관리 로직을 useCalendar로 이동 #10

--- src/hooks/useCalendar.ts
+++ src/hooks/useCalendar.ts
@@ -1,8 +1,12 @@
-import { RefObject } from 'react';
+import { RefObject, useEffect, useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
+import moment, { Moment } from 'moment';
 
 // Calendar/DatePicker에서 공통적으로 사용되는 네비게이션 버튼 클릭 이벤트를 사용하기 위한 훅
 const useCalendar = (calendarRef: RefObject<FullCalendar>) => {
+  const [date, setDate] = useState<Moment>(
+    moment(calendarRef.current?.getApi().getDate()),
+  );
   const handleNavClick = async (type: 'prev' | 'today' | 'next') => {
     const api = calendarRef.current?.getApi();
     if (api) {
@@ -13,10 +17,20 @@ const useCalendar = (calendarRef: RefObject<FullCalendar>) => {
       } else {
         api.next();
       }
+      setDate(moment(api.getDate()));
       return api;
     }
   };
+
+  useEffect(() => {
+    const api = calendarRef.current?.getApi();
+    if (api) {
+      setDate(moment(api.getDate()));
+    }
+  }, []);
+
   return {
+    date,
     handleNavClick,
   };
 };
