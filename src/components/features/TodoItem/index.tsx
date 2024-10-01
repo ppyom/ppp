e54ee21659ef1390d38847e2bce4fee7eaf10a8f
ppyom <ouob77@kakao.com>
2024-10-01 12:05:50 +0900

    design: Kebab 메뉴 적용 #3

--- src/components/features/TodoItem/index.tsx
+++ src/components/features/TodoItem/index.tsx
@@ -9,12 +9,28 @@ import styles from './styles.module.css';
 interface Props extends Todo {}
 
 const TodoItem = ({ title, deadline, isCompleted }: Props) => {
-  const [editMode] = useState(false);
+  const [kebabOpen, setKebabOpen] = useState(false);
+  const [editMode, setEditMode] = useState(false);
   const handleMenuOpen = () => {
-    // TODO 케밥 메뉴 오픈
+    setKebabOpen(true);
   };
   const handleComplete = () => {
     // TODO 완료 처리
+  };
+  const handleKebabMenuClick = (type: 'edit' | 'delete') => {
+    if (type === 'edit') {
+      setEditMode(true);
+    } else {
+      // TODO 삭제 처리
+    }
+    setKebabOpen(false);
+  };
+  const handleDeadlineChange = () => {
+    // TODO Deadline 설정
+  };
+  const handleEdit = () => {
+    // TODO 저장
+    setEditMode(false);
   };
   return (
     <div
@@ -22,22 +38,38 @@ const TodoItem = ({ title, deadline, isCompleted }: Props) => {
       onClick={handleComplete}
     >
       <div className={styles.title}>
-        <p>{title}</p>
+        {editMode ? (
+          <input value={title} onChange={() => {}} />
+        ) : (
+          <p>{title}</p>
+        )}
       </div>
       {deadline && (
-        <p className={styles.deadline}>
+        <p
+          className={styles.deadline}
+          onClick={editMode ? handleDeadlineChange : () => {}}
+        >
           <FaClock />
           <span>{deadline}</span>
         </p>
       )}
       <div className={styles.buttons}>
-        <Button
-          className={styles.menu}
-          Icon={FiMoreVertical}
-          onClick={handleMenuOpen}
-        />
-        {editMode && <Button className={styles.ok} Icon={FiCheck} active />}
+        {editMode ? (
+          <Button className={styles.ok} Icon={FiCheck} active />
+        ) : (
+          <Button
+            className={styles.menu}
+            Icon={FiMoreVertical}
+            onClick={handleMenuOpen}
+          />
+        )}
       </div>
+      {kebabOpen && (
+        <div className={styles.kebab}>
+          <button onClick={() => handleKebabMenuClick('edit')}>수정</button>
+          <button onClick={() => handleKebabMenuClick('delete')}>삭제</button>
+        </div>
+      )}
     </div>
   );
 };
