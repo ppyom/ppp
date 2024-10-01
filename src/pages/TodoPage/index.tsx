f98cd6544c40a9071149ba3ffce0c2daac46da01
ppyom <ouob77@kakao.com>
2024-10-01 09:39:46 +0900

    design: 할 일 페이지 기본 레이아웃 스타일 적용 #3

--- src/pages/TodoPage/index.tsx
+++ src/pages/TodoPage/index.tsx
@@ -1,5 +1,12 @@
+import PageLayout from '@/components/layout/PageLayout';
+import styles from './styles.module.css';
+
 const TodoPage = () => {
-  return <>할 일</>;
+  return (
+    <PageLayout title="할 일" hideTitle className={styles.page}>
+      할 일
+    </PageLayout>
+  );
 };
 
 export default TodoPage;
