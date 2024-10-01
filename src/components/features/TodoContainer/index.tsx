961037764eb0498e749b5d18987852a991f5bbcb
ppyom <ouob77@kakao.com>
2024-10-01 10:08:24 +0900

    design: TodoContainer 컴포넌트 생성 #3

---
+++ src/components/features/TodoContainer/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+const TodoContainer = () => {
+  return (
+    <section className={styles.container}>
+      <h3>ToDoList</h3>
+    </section>
+  );
+};
+
+export default TodoContainer;
--- src/pages/TodoPage/index.tsx
+++ src/pages/TodoPage/index.tsx
@@ -1,10 +1,11 @@
 import PageLayout from '@/components/layout/PageLayout';
+import TodoContainer from '@/components/features/TodoContainer';
 import styles from './styles.module.css';
 
 const TodoPage = () => {
   return (
     <PageLayout title="할 일" hideTitle className={styles.page}>
-      할 일
+      <TodoContainer />
     </PageLayout>
   );
 };
