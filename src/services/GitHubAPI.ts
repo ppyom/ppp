116a5ee6315d7de25e1fea0909d9e7699fc10d8d
ppyom <ouob77@kakao.com>
2024-10-07 10:51:31 +0900

    fix: GitHub 레파지토리 2달 => 1달로 수정
    * 기획서의 내용과 동일하게 수정

--- src/services/GitHubAPI.ts
+++ src/services/GitHubAPI.ts
@@ -6,7 +6,7 @@ class GitHubAPI {
   private static baseURL = `${import.meta.env.VITE_SERVER_URL}/github`;
   static async getRepositories<T = { data: GitHub[] }>(): Promise<T> {
     const response = await axios.get<T>(
-      `${this.baseURL}/repositories?date=${moment().subtract(2, 'month').format('YYYY-MM-DD')}`,
+      `${this.baseURL}/repositories?date=${moment().subtract(1, 'month').format('YYYY-MM-DD')}`,
     );
     return response.data;
   }
